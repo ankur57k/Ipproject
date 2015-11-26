5e26448920c6519179c919604cd922c774d670f4
tSushant <tongaonkarsushant@gmail.com>
2015-11-25 19:58:16 -0500

    Threading

--- controlApp.py
+++ controlApp.py
@@ -8,52 +8,59 @@ import time
 import socket
 import os
 import thread
+from Queue import Queue
 
-def getEdgeStat(flag1,stat1):
+def getEdgeStat(threadname,flag1,edgestat):
 	gateway_ip = '192.168.4.1'
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((gateway_ip,9880))	
 	s.send("GET_")
 	l = s.recv(1024)
-	stat1 = float(l)
+	edgestat.put(float(l))
 	s.shutdown(socket.SHUT_RDWR)
 	s.close()
-	flag1 = 1 
+	flag1.put(1)
 
-def getCloudStat(flag2,stat2):
+def getCloudStat(threadname,flag2,cloudstat):
 	gateway_ip = '192.168.4.1'
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((gateway_ip,9881))	
 	s.send("GET_")
 	l = s.recv(1024)
-	stat2 = float(l)
+	cloudstat(float(l))
 	s.shutdown(socket.SHUT_RDWR)
 	s.close()
-	flag2 = 1 
+	flag2.put(1)
 
 
 if __name__ = '__main__':
 	NewSwitch = InitSwitch("First")
         core.register("First",NewSwitch)
 	default_route = 0
+	flag1 = Queue()
+	flag2 = Queue()
+	edgestat = Queue()
+	cloudstat = Queue()
 	while True:
-		flag1 = 0
-		flag2 = 0
-		edgestat=0
-		cloudstat=0
-		thread.start_new_thread(getEdgeStat, (flag1,edgestat,))
-		thread.start_new_thread(getCloudStat, (flag2,cloudstat,))
-		getEdgeStat(flag1,stat2)
-		getCloudStat(flag2,stat2)
+		flag1.put(0)
+		flag2.put(0)
+		thread.start_new_thread(getEdgeStat, ("Thread-1",flag1,edgestat,))
+		thread.start_new_thread(getCloudStat, ("Thread-2",flag2,cloudstat,))
 
-		while(!flag1 || !flag2):
+		while(0 == flag1.get()):
+			pass
+		edgeload = edgestat.get()
 
-		if(edgestat > cloudstat):
+		while(0 == flag2.get()):
+			pass
+		cloudload = cloudstat.get()		
+	
+		if(edgeload > cloudload):
 			if (!default_route):
 				#call function to push default_route - route to cloud 
 		else:
-			if(defaulr_route):
+			if(default_route):
 				#call function to push no default_route - route to edge
-
-	
+		time.sleep(2)
+			
 
